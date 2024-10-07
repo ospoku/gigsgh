81b59d52085813354693d3223970269fc6f8f332
oseipoku <ospoku@gmail.com>
2024-10-07 18:25:59 +0000

    updating splash screen

---
+++ lib/pages/splash.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+    
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    
+    _controller.forward();
+
+    // Navigate to next screen after the animation completes
+    Timer(Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => Home()),  // Replace with your next screen
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: ScaleTransition(
+            scale: _animation,
+            child: Image.asset('assets/logo.png'),  // Use your logo image here
+          ),
+        ),
+      ),
+    );
+  }
+}
